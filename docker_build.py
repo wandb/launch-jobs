5a76fabeb3f9dc39ce6b50cc3b6e8a722d4cc605
gtarpenning <griffin.tarpenning@wandb.com>
2023-06-05 21:54:31 -0700

    format image name no /

--- docker_build.py
+++ docker_build.py
@@ -216,10 +216,12 @@ if __name__ == "__main__":
         if GIT_BRANCH_DIRTY == "master":
             extra_write_tags.append("latest")
 
+        image_formatted = image.replace('/', '_')
+
         build(
-            image=f"wandb/job_{image}",
+            image=f"wandb/job_{image_formatted}",
             context_path=os.path.join(repo, "jobs", image),
             dockerfile=os.path.join(repo, "jobs", image, "Dockerfile"),
-            cache_from_image=f"wandb/{image}",
+            cache_from_image=f"wandb/{image_formatted}",
             extra_write_tags=extra_write_tags,
         )
