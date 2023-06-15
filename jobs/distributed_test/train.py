29b64d54ed666bd7b2c6217e78ba345548aa306b
Ben Sherman <bcanfieldsherman@gmail.com>
2023-06-15 14:56:16 -0700

    beefed the model up a bit

--- jobs/distributed_test/train.py
+++ jobs/distributed_test/train.py
@@ -37,10 +37,12 @@ def mnist_train():
     wandb.init(config=DEFAULT_CONFIG)
     transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
     dataset = FakeData(
-        size=1000, image_size=(3, 64, 64), num_classes=196, transform=transforms
+        size=10000, image_size=(3, 256, 256), num_classes=196, transform=transforms
     )
     model = torch.nn.Sequential(
-        torch.nn.Linear(12288, 1024),
+        torch.nn.Linear(196608, 4096),
+        torch.nn.ReLU(),
+        torch.nn.Linear(4096, 1024),
         torch.nn.ReLU(),
         torch.nn.Linear(1024, 196),
     )
