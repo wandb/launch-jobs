1a632694a1351a3865cd76c960e1334fe87f09d8
Chris Van Pelt <vanpelt@wandb.com>
2024-08-13 12:20:30 -0400

    Trying submitit

---
+++ jobs/slurm/submitit/main.py
@@ -0,0 +1,25 @@
+import asyncio
+import random
+import submitit
+import time
+
+def slow_multiplication(x, y):
+    time.sleep(x*y)
+    return x*y
+
+async def main():
+    executor = submitit.AutoExecutor(folder="logs")
+    executor.update_parameters(timeout_min=1, slurm_partition="dev")
+
+    # await a single result
+    job = executor.submit(slow_multiplication, 10, 2)
+    await job.awaitable().result()
+
+    # print results as they become available
+    jobs = [executor.submit(slow_multiplication, k, random.randint(1, 4)) for k in range(1, 5)]
+    for aws in asyncio.as_completed([j.awaitable().result() for j in jobs]):
+        result = await aws
+        print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ jobs/slurm/submitit/submitit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SBATCH --job-name=example1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+CMD="python submitit.py"
+srun --jobid $SLURM_JOBID bash -c "$CMD"
