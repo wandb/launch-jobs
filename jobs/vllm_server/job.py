140307b06eb8a1fd12a67a3dc995b1575871ebc6
Nicholas Pun <182540099+nicholaspun-wandb@users.noreply.github.com>
2025-06-11 11:56:14 -0700

    fix

--- jobs/vllm_server/job.py
+++ jobs/vllm_server/job.py
@@ -2,15 +2,11 @@ import wandb
 import os
 import json
 import subprocess
+import threading
 
-settings = wandb.Settings(disable_git=True)
-
-with wandb.init(settings=settings) as run:
-    run.log({"hello": "world"})
-    
 wandb_config = json.loads(os.environ.get("WANDB_CONFIG"))
 
-model_name = wandb_config.get("model_name")
+model_name = wandb_config.get("model")
 artifact_path = wandb_config.get("artifact_path")
 
 if not model_name and not artifact_path:
@@ -20,20 +16,24 @@ wandb_api = wandb.Api()
 
 model = wandb_api.artifact(artifact_path).download() if artifact_path else model_name
 
+args = [
+    "vllm",
+    "serve",
+    model,
+    "--api-key",
+    "token-abc123",
+    "--trust-remote-code",
+    "--enable-chunked-prefill",
+    "--max-num-batched-tokens",
+    "1024",
+    "--chat-template",
+    "chat_template.jinja"
+]
+
+print(f"Starting VLLM server with args: {args}")
+
 vllm_server = subprocess.Popen(
-    [
-        "vllm",
-        "serve",
-        model,
-        "--api-key",
-        "token-abc123",
-        "--trust-remote-code",
-        "--enable-chunked-prefill",
-        "--max_num_batch_tokens",
-        "1024",
-        "--chat-template",
-        "chat_template.jinja"
-    ],
+    args,
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
     text=True,
@@ -46,8 +46,6 @@ def log_output(pipe, prefix):
     for line in pipe:
         print(f"{prefix}: {line.strip()}")
 
-import threading
-
 stdout_thread = threading.Thread(
     target=log_output, args=(vllm_server.stdout, "vllm stdout")
 )
@@ -59,12 +57,11 @@ stderr_thread.daemon = True
 stdout_thread.start()
 stderr_thread.start()
 
+print("VLLM server starting ...")
 
-
-
-
-
-    
-
-
-
+try:
+    vllm_server.wait()
+except KeyboardInterrupt:
+    print("Received interrupt signal, shutting down...")
+    vllm_server.terminate()
+    vllm_server.wait()
